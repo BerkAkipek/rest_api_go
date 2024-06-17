62253e1d09533a02a8179103a4c75994d4dcc635
BerkAkipek <berk.akipek.96@gmail.com>
2024-06-17 14:41:19 +0300

    First end point created

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/BerkAkipek/rest_api_go/db"
+	"github.com/BerkAkipek/rest_api_go/models"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse Request Data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 
 }
